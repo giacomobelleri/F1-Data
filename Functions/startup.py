3aab71fd5221f64d479dcec3016b5f0c6a7440be
Giacomo Belleri <giaco.belleri00@gmail.com>
2024-02-01 15:47:50 +0100

    Added wall time to startup.py

--- Functions/startup.py
+++ Functions/startup.py
@@ -1,10 +1,21 @@
-import fastf1 as ff1 # Import fastf1
+# Import all the necessary modules and set up the cache directory
 import os
+import time
+import fastf1 as ff1 # Import fastf1
+
+# Saves start time(s):
+start_time = time.time()
 
 # Sets global cache directory to "Cache"
-os.makedirs('Cache', exist_ok=True) # Creates the cache folder if it doesn't exist
-ff1.Cache.enable_cache('Cache')     # Enables the cache on "Cache"
+cache_dir = "Cache"
+os.makedirs(cache_dir, exist_ok=True) # Creates the cache folder if it doesn't exist
+ff1.Cache.enable_cache(cache_dir)     # Enables the cache on "Cache"
+
+# Saves end time(s):
+end_time = time.time()
 
 if __name__ == '__main__':
-    print("Cache drirectory created. \n" +
-          f"Cache directory: ", ff1.Cache.directory) 
+    print(f"\n{__file__.split("\\")[-1]} ran succesfully. \n")
+    print("Cache created succesfully. \n" +
+          f" -> Cache directory: {cache_dir} \n" +
+          f" -> Cache creation time: {(end_time - start_time):.5f} s \n")
